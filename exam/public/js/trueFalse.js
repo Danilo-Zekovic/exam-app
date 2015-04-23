cae88e4e8cf8e110acb081707afbb09952a579ff
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-04-22 20:32:12 -0400

    Started the logic

--- exam/public/js/trueFalse.js
+++ exam/public/js/trueFalse.js
@@ -5,4 +5,20 @@
  * Create the ture false questions that will be read freom tfJson.js file
  */
 
+true_false_form = function( jqueryMap, visited ) {
 
+  console.log("true_false_form " + visited);
+  
+  var data = TRUEFALSE;  // set data to the data from tfJson.js 
+
+  // array to keep the corect answears
+  var solutions = [];
+
+
+  if (visited){
+    jqueryMap.$tf.show();
+  }else{
+    jqueryMap.$tf.show();
+    jqueryMap.$tf.append('<p>Danilo</p>');
+  }
+}
