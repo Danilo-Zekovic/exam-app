033205129d802a5b0aa0e1efacbe41d3ae9962d4
Craig <cav9871@saintjoe.edu>
2015-04-23 15:07:48 -0400

    I have it checking the answers and printing out in the log if you are correct or not

--- exam/public/js/choice.js
+++ exam/public/js/choice.js
@@ -16,6 +16,7 @@ multchoice_form = function( jqueryMap, visited ) {
    var numOfQuestions = 7;
    var nums = 1;
    var count =0;
+   var answers = [];
   // array to keep the corect answers
   //var solutions = [];
 
@@ -35,7 +36,8 @@ for (var i = 0;i < numOfQuestions; i++) {
 //jqueryMap.$multchoice.append('<div class="craig"></div>');
   var qtion = data[i];
   var question = qtion.text;
-  var answer = qtion.answer;
+  answers[i] = qtion.answer;
+  console.log('answer for: '+ answers[i] );
      console.log(JSON.stringify(question)); 
     
 
@@ -55,7 +57,7 @@ for (var i = 0;i < numOfQuestions; i++) {
    	
    	var choices = qtion.decoys[j];
    	$('.row:last').append('<div class="col-xs-12 col-md-10"></div>');
-      $('.col-md-10:last').append('<input type="radio" aria-label="..." class="rad" id="' +count +'" name="num'+i+'" value="'+choices+'"><label class="lab"></label><br>');
+      $('.col-md-10:last').append('<input type="radio" aria-label="..." class="rad" id="' +count +'" name="'+ i+ '" value="'+choices+'" ><label class="lab"></label><br>');
        $('.lab:last').html(choices);
    
         count++;
@@ -69,79 +71,51 @@ for (var i = 0;i < numOfQuestions; i++) {
     alert($("input[name='num0']:checked", '.rad').());
   });
 */
-}
+var userAns = [];
+$(function() {
 
-}
-  /*
+  $('input:radio').change(function () {
+    var temp = $(this).attr('name');
+    if($(this).prop('checked') == true) {
+    console.log($(this).val());
+    userAns[temp] = $(this).val();
+    console.log(userAns); 
+    }
+  });
+});
+
+
+
+  
 var buttonString = '<div class="row">'
 	                 + '<div class="col-xs-12 submit">'
-			   + '<button type="button" class="btn btn-primary btn-block submit-btn-fillin">Submit</button>'
+			   + '<button type="button" class="btn btn-primary btn-block submit-btn-choice">Submit</button>'
 			 + '</div>'
 		       + '</div>';
     jqueryMap.$multchoice.append(buttonString);
-    console.log(solutions.toString());
+    console.log(answers.toString());
     
-    $('.submit-btn-choice').click({solutions:solutions},gradeChoice);
-
-  } // end if else
+    $('.submit-btn-choice').click(function() {
+      
+          for( var i =0; i < numOfQuestions; i++) {
+    if(userAns[i] === answers[i]) {
+      console.log('Correct');
+    }
+    else {
+      console.log('Wrong');
+    }
+}
+});
+     
+  // end if else
 } // end match_form
-*/
+}
 
 
 
 
-/*var gradeChoice = function( event ) {
-  var solutions = event.data.solutions;
+var gradeChoice = function( event ) {
+  //var answers = event.data.answers;
   //console.log("Submit Clicked " + solutions.toString());
 
-  var ans = []; // array to store user answers
-  var i = 0;    // to keep the place to store the value in ans array
-  // move through all the input lists and get the selected value
-  $('.choice-ans').each(function() {
-    ans[i] = $(this).val();
-    //console.log($(this).val());
-    i++;
-  });
-
-  var wrong = 0;
-  var correct = 0;
-  var prev_correct = 0;
-
-  // check are the answers correct
-  // unanswered questions will be considered wrong
-  for (var j = 0; j < solutions.length; ++j){
-    // test for all possible solutions in second dimension
-    for (var k = 0; k < solutions[j].length; ++k) {
-      //console.log(solutions[j][k]);
-      // found answer
-      if (ans[j] == solutions[j][k]){
-        prev_correct = correct;
-        correct++;
-      } 
-      // if the answer is not last possible solution and a correct answer wasn't already found
-      else if (k == solutions[j].length - 1 && ans[j] != solutions[j][k] && prev_correct == correct) {
-        //console.log(k + "   " +solutions[j].length);
-        //console.log("hey dude look here -> " + ans[j] + " =? " + solutions[j][k]);
-        wrong++;
-      } else {
-        continue;
-      }
-    } // for k
-    // set the prev_correct to be the same as correct before checking next question
-    prev_correct = correct;
-  } // for j
-
-  //console.log("correct answers: " + correct + " wrong ans: " + wrong);
-  $('.fillin').empty();
-  $('.fillin').append(
-     '<div class="row">'
-      + '<div class="col-xs-12">'
-        + '<label class="fillin-grade"></label>'
-      + '</div>'
-    + '</div>'
-  );
-  var str = 'Correct: ' + correct + ', wrong: ' + wrong;
-  //console.log(str);
-  $('.fillin-grade').html(str);
 }  // end grade
-*/
